c54bca44432bf6035c4e02550b72d37eaf48a138
Dino Scheidt <github@dino-scheidt.de>
2017-07-21 21:14:56 +0200

    ⬆️ Add missing symbol

--- _demo_/app/component/button/story.js
+++ _demo_/app/component/button/story.js
@@ -4,6 +4,8 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
 
 import Button from '@component/button';
 import CenterView from '@container/centerView';
