e33f745faed1ba557ecebb2a575394be4189e66e
Dino Scheidt <github@dino-scheidt.de>
2017-08-25 17:29:57 +0200

    🔨 Refactor to flat folder structure

--- app/main.js
+++ app/main.js
@@ -5,13 +5,12 @@ import { AppRegistry, StyleSheet, View, Text } from 'react-native';
 // https://blog.callstack.io/a-cure-for-relative-requires-in-react-native-2b263cecf0f6
 import Welcome from "@scene/welcome";
 import CenterView from "@container/centerView";
+import Scanner from "@container/scanner";
 
 export default class App extends Component {
   render() {
     return (
-      <CenterView>
-        <Welcome />
-      </CenterView>
+      <Scanner />
     );
   }
 }
